ddf95e40df19e02992b18878b948cdfb8a5f9038
Saurabh Garkoti <garkotisaurabh99@gmail.com>
2026-06-29 09:49:32 +0530

    complete project

--- Tic-Tac-Toe/script.js
+++ Tic-Tac-Toe/script.js
@@ -1,5 +1,8 @@
 let boxes = document.querySelectorAll(".box");
+let resetbtn = document.querySelector(".reset-btn");
+let msg = document.querySelector(".msg");
 
+turn0 = false;
 let winPatterns = [
   [0, 1, 2],
   [3, 4, 5],
@@ -10,7 +13,7 @@ let winPatterns = [
   [0, 4, 8],
   [2, 4, 6],
 ];
-turn0 = true;
+
 boxes.forEach((box) => {
   box.addEventListener("click", () => {
     if (turn0 === true) {
@@ -21,6 +24,52 @@ boxes.forEach((box) => {
       turn0 = true;
     }
     box.disabled = true;
-    count++;
+    checkWinner();
+    checkDraw();
   });
 });
+
+const checkDraw = () => {
+  for(let box of boxes){
+    if(box.innerText === ""){
+      return;
+    }
+  }
+  msg.innerText = "It is a Draw";
+  msg.classList.remove("hide");
+};
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    let pos1 = boxes[pattern[0]].innerText;
+    let pos2 = boxes[pattern[1]].innerText;
+    let pos3 = boxes[pattern[2]].innerText;
+
+    if (pos1 !== "" && pos2 !== "" && pos3 !== "") {
+      if (pos1 === pos2 && pos2 === pos3) {
+        showWinner(pos1);
+      }
+    }
+  }
+};
+const showWinner = (winner) => {
+  msg.innerText = `Congratulation, winner is ${winner}`;
+  msg.classList.remove("hide");
+  disableboxes();
+};
+const disableboxes = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
+
+resetbtn.addEventListener("click", (e) => {
+  turn0 = false;
+  enableboxes();
+});
+const enableboxes = () => {
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+    msg.classList.add("hide");
+  }
+};
